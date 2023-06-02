fd4f82957c0feca47d65b99d8da96c37722ab81e
Camilla <83785401+CamillaHanninen@users.noreply.github.com>
2023-06-02 21:58:01 +0300

    Update fs_demo.js, create file with fs.writeFile

--- Coursework/Nodejs/reference/fs_demo.js
+++ Coursework/Nodejs/reference/fs_demo.js
@@ -2,7 +2,17 @@ const fs = require('fs');
 const path = require('path');
 
 // Create a folder
-fs.mkdir(path.join(__dirname, '/test'), {}, err => {
-  if(err) throw err;
-    console.log('Folder created');
-});
+// fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+//  if(err) throw err;
+//    console.log('Folder created');
+//});
+
+// Create and write to a file
+fs.writeFile(
+  path.join(__dirname, '/test', 'hello.txt'),
+  'Hello !',
+  err => {
+    if(err) throw err;
+    console.log('File written to');
+  }
+);
