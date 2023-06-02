ffb18699972504f66b8761630cb4cdf64d88c4f9
Camilla <83785401+CamillaHanninen@users.noreply.github.com>
2023-06-02 08:38:24 +0300

    Update index.js (removed source link comments, already in readme.md)

--- Coursework/Nodejs/index.js
+++ Coursework/Nodejs/index.js
@@ -1,9 +1,3 @@
-/*  Node.js tutorial for course
-    source tutorial: https://www.youtube.com/watch?v=fBNz5xF-Kx4
-    source github: https://github.com/bradtraversy/node_crash_course
-*/
-
-
 const Person require('./person');
 
 const person1 = new Person('John Doe', 30);
