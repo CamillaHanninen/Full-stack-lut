9167a9de212a510592ac3988fe0ffc4b6af82cee
Camilla <83785401+CamillaHanninen@users.noreply.github.com>
2023-06-02 08:52:51 +0300

    Create path_demo.js
    demonstrating the use of node's path module

---
+++ Coursework/Nodejs/reference/path_demo.js
@@ -0,0 +1,25 @@
+const path = require('path);
+                     
+// Base file name
+console.log(path.basename(__filename));
+// path_demo.js
+
+// Directory name
+console.log(path.dirname(__filename));
+// /Users/.../Coursework/Nodejs/reference
+// Or use just __dirname
+
+// File extension
+console.log(path.extname(__filename));
+// .js
+
+// Create path object
+console.log(path.parse(__filename).base);
+// path-demo.js
+
+// Concatenate paths (../test/hello.html)
+console.log(path.join(__dirname, "test", "hello.html"));
+// /Users/.../Coursework/Nodejs/reference/test/hello.html
+
+
+
