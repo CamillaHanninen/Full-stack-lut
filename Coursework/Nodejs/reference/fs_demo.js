79e6e9a8fcd6c06a3dfa56554a162676c2d251a0
Camilla <83785401+CamillaHanninen@users.noreply.github.com>
2023-06-03 19:04:56 +0300

    Update fs_demo.js
    added method fs.appendFile()

--- Coursework/Nodejs/reference/fs_demo.js
+++ Coursework/Nodejs/reference/fs_demo.js
@@ -7,12 +7,25 @@ const path = require('path');
 //    console.log('Folder created');
 //});
 
-// Create and write to a file
+// Create and write to a file (clatest can overwrite files)
 fs.writeFile(
   path.join(__dirname, '/test', 'hello.txt'),
   'Hello !',
   err => {
     if(err) throw err;
     console.log('File written to');
+    
+    // Appends to a file
+    fs.appendFile(
+      path.join(__dirname, '/test', 'hello.txt'),
+      '<3 Node.js,
+      err => {
+        if(err) throw err;
+        console.log('File written to');
+      }
+    );
   }
 );
+
+// Appends to a file
+//fs.appendFile( ..
