1beeaf7a8ea8c4ec23f1aea020976da86ececde6
Camilla <83785401+CamillaHanninen@users.noreply.github.com>
2023-06-03 19:48:06 +0300

    Update os_demo.js
    os. methods demo

--- Coursework/Nodejs/reference/os_demo.js
+++ Coursework/Nodejs/reference/os_demo.js
@@ -1,1 +1,28 @@
+const os = require('os');
+
+// Platforms
+console.log(os.platform());
+// Returns 'win32'
+
+// CPU core info
+console.log(os.cpus());
+
+// Free memory
+console.log(os.freemem());
+
+// Total memory
+console.log(os.totalmem());
+
+
+
+
+
+
+
+
+
+
+
+
+
 
