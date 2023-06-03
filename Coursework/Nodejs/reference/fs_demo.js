cceb29516da22ff4496938ccc1942b9ba8cafe93
Camilla <83785401+CamillaHanninen@users.noreply.github.com>
2023-06-03 19:21:52 +0300

    Update fs_demo.js
    added fs.rename()

--- Coursework/Nodejs/reference/fs_demo.js
+++ Coursework/Nodejs/reference/fs_demo.js
@@ -5,27 +5,46 @@ const path = require('path');
 // fs.mkdir(path.join(__dirname, '/test'), {}, err => {
 //  if(err) throw err;
 //    console.log('Folder created');
-//});
+// });
 
 // Create and write to a file (clatest can overwrite files)
-fs.writeFile(
-  path.join(__dirname, '/test', 'hello.txt'),
-  'Hello !',
-  err => {
-    if(err) throw err;
-    console.log('File written to');
+// fs.writeFile(
+//   path.join(__dirname, '/test', 'hello.txt'),
+//   'Hello !',
+//   err => {
+//     if(err) throw err;
+//     console.log('File written to');
     
     // Appends to a file
-    fs.appendFile(
-      path.join(__dirname, '/test', 'hello.txt'),
-      '<3 Node.js,
-      err => {
-        if(err) throw err;
-        console.log('File written to');
-      }
-    );
+//     fs.appendFile(
+//       path.join(__dirname, '/test', 'hello.txt'),
+//       '<3 Node.js,
+//       err => {
+//         if(err) throw err;
+//         console.log('File written to');
+//       }
+//     );
+//   }
+// );
+
+// Reads a file
+// fs.readFile(
+//   path.join(__dirname, '/test', 'hello.txt'),
+//   'utf8',
+//   (err, data) => {
+//     if(err) throw err;
+//     console.log(data);
+//   });
+// Prints 'Hello ! <3 Node.js' from hello.txt
+    
+// Renames a file    
+fs.rename(
+  path.join(__dirname, '/test', 'hello.txt'),
+  path.join(__dirname, '/test', 'newHello.txt'),
+  (err) => {
+    if(err) throw err;
+    console.log('File renamed');
   }
 );
-
-// Appends to a file
-//fs.appendFile( ..
+    
+    
