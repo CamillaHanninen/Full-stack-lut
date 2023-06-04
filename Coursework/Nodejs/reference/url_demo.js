45d8b05fcfc2b61e16f40e59fa78e711b533de28
Camilla <83785401+CamillaHanninen@users.noreply.github.com>
2023-06-04 21:00:37 +0300

    Update url_demo.js
    Demonstrates the use of some of the URL module methods

--- Coursework/Nodejs/reference/url_demo.js
+++ Coursework/Nodejs/reference/url_demo.js
@@ -1,1 +1,34 @@
+const url = require('url');
 
+const myUrl = new URL('http://mywebsite.com/hello.html?id=100&status=active')
+
+// Serialized URL
+console.log(myUrl.href);
+console.log(myUrl.toString);
+// Same output
+
+// Host (root domain)
+console.log(myUrl.host);
+
+// Hostname (doesn't give port)
+console.log(myUrl.hostname);
+
+// Pathname
+console.log(myUrl.pathname);
+// '/hello.html'
+
+// Serialized query
+console.log(myUrl.search);
+// '?id=100&status=active'
+
+// Parameters objects
+console.log(myUrl.searchParams);
+// Gives object with the values of the id, status etc above
+
+// Add parameters
+myUrl.searchParams.append('abc', '123')=;
+console.log(myUrl.searchParams);
+// Also gives the appended parameters
+
+// Loop through the parameters
+myUrl.searchParams.forEach((val, name) => console.log(`${name}: ${val}`));
